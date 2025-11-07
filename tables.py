f61d7e63d6ac90906fcb2d5175724f47d81b1060
Talha Rana <mtalharana093@gmail.com>
2025-11-07 19:20:41 +0500

    create more than one tables

--- tables.py
+++ tables.py
@@ -8,12 +8,22 @@ metadata = MetaData()
 
 ## Creating using Table
 ## import Metadata, table and Columns
+# User table
 users = Table(
   "users",
   metadata,
   Column("id", Integer, primary_key=True),
   Column("name", String(length=50), nullable=False),
   Column("email", String, nullable=False, unique=True),
+  Column("phone", Integer, nullable=False, unique=True),
+)
+address = Table(
+  "address",
+  metadata,
+  Column("id", Integer, primary_key=True),
+  Column("street", String(length=50), nullable=False),
+  Column("dist", String, nullable=False, unique=True),
+  Column("country", String, nullable=False, unique=True),
 )
 
 
