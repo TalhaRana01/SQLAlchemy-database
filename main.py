3747cca88aa58a3023db84bc91f3bb5d13769dd9
Talha Rana <mtalharana093@gmail.com>
2025-11-08 13:43:03 +0500

    read all users

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from tables import create_tables
-from services import create_user, create_post
+from services import create_user, create_post, get_user_by_id, get_all_users
 
 
 
 # Craete Tables by calling createTables from tables.py
 create_tables()
 
-create_user("Talha Rana", "talharana01@gmail.com")
+# Create User
+# create_user("Talha Rana", "taharana01@gmail.com")
+# create_user("Ali Rana", "This is a Ali Rana")
 
-create_post(1, "Talha Rana AI Engineer", "This is a Talha Rana AL ML Engineer")
+# Create Post
+# create_post(1, "Talha Rana AI Engineer", "This is a Talha Rana AL ML Engineer")
+# create_post(2, "Ali ahmed", "This is a businessmen")
+
+# Read single user data
+# print(get_user_by_id(1))
+
+
+print(get_all_users())
--- services.py
+++ services.py
@@ -12,6 +12,7 @@ def create_user(name: str, email:str):
     stmt = insert(users).values(name=name, email=email)
     conn.execute(stmt)
     conn.commit()
+
     
 # Insert or create post 
 def create_post(user_id: int, title:str, content: str):
@@ -19,3 +20,19 @@ def create_post(user_id: int, title:str, content: str):
     stmt = insert(posts).values(user_id=user_id, title=title, content=content)
     conn.execute(stmt)
     conn.commit()
+
+
+# Get single users 
+def get_user_by_id(user_id: int):
+  with engine.connect() as conn:
+    stmt = select(users).where(users.c.id == user_id)
+    result = conn.execute(stmt).first()
+    return result
+
+# Get all users
+def get_all_users():
+  with engine.connect() as conn:
+    stmt = select(users)
+    result = conn.execute(stmt).fetchall()
+    return result
+   
