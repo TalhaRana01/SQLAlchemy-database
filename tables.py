0c3000ae631d101ed96c19bde0392a47cd7df8c2
Talha Rana <mtalharana093@gmail.com>
2025-11-08 12:19:17 +0500

    many to many relation

--- tables.py
+++ tables.py
@@ -36,28 +36,36 @@ users = Table(
 ##--------------------
 # One to One Relation
 ##--------------------
-profile = Table(
-  "profile",
+# profile = Table(
+#   "profile",
+#   metadata,
+#    Column("id", Integer, primary_key=True),
+#    Column("user_id", Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=True, unique=True),
+#    Column("bio", String, nullable=False),
+#    Column("address", String, nullable=False),
+# )
+
+
+##---------------------------
+## Many to Many Relation 
+##---------------------------
+
+address = Table(
+  "address",
   metadata,
-   Column("id", Integer, primary_key=True),
-   Column("user_id", Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=True, unique=True),
-   Column("bio", String, nullable=False),
-   Column("address", String, nullable=False),
+  Column("id", Integer, primary_key=True),
+  Column("street", String(length=50), nullable=False),
+  Column("country", String, nullable=False, unique=True),
+  
 )
 
-
-##---------------------------
-## Table as a practice
-##---------------------------
-
-# address = Table(
-#   "address",
-#   metadata,
-#   Column("id", Integer, primary_key=True),
-#   Column("street", String(length=50), nullable=False),
-#   Column("dist", String, nullable=False, unique=True),
-#   Column("country", String, nullable=False, unique=True),
-# )
+user_address_association  = Table(
+  "user_address_association",
+  metadata,
+  Column("user_id", Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True),
+  Column("address_id", Integer, ForeignKey("address.id", ondelete="CASCADE"), primary_key=True),
+  
+)
 
 
 ## Craeting Table in Database
